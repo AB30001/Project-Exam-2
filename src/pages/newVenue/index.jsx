fad5dd0e9daded31116d2818c126387ab6cb02c1
Arnas <arnasbalciunas@gmail.com>
2025-02-23 21:42:15 +0100

    Pages:newVenue

---
+++ src/pages/newVenue/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import * as c from "../../modules/styles/common";
+import NewVenue from "../../modules/components/newVenueForm";
+
+function App(props) {
+  const title = document.querySelector("title");
+  title.innerText = "Holidaze | Create a venue";
+  return (
+    <div>
+      <c.MainHeading>Create a venue</c.MainHeading>
+      <NewVenue />
+    </div>
+  );
+}
+
+export default App;
