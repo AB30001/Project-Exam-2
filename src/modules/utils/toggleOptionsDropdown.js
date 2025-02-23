0ae1e9c786c8a3cfd2360e59671a6b73560acc0d
Arnas <arnasbalciunas@gmail.com>
2025-02-23 21:38:42 +0100

    scrollFix, toggleOptionsDropdown

---
+++ src/modules/utils/toggleOptionsDropdown.js
@@ -0,0 +1,15 @@
+/**
+ * A function to toggle the options dropdown
+ * @param {object} e The event object
+ */
+function handleOptions(e) {
+  e.preventDefault();
+  const dropdown = document.querySelectorAll(".options-dropdown");
+  const newArray = Array.from(dropdown);
+  const targetDropdown = newArray.filter(
+    (item) => item.dataset.id === e.target.dataset.id
+  );
+  targetDropdown[0].classList.toggle("show");
+}
+
+export default handleOptions;
